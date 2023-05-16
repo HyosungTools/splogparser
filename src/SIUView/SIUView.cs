c0661877f99c2bd09305e2f8279b68a110353b39
davidewan <davidewan@live.com>
2023-05-16 10:51:12 -0400

    Added SIUView Added first cut SIUView to report on the safe door (open, close). Ramji needed this for a log analysis.

---
+++ src/SIUView/SIUView.cs
@@ -0,0 +1,27 @@
+﻿using Contract;
+using Impl;
+using System.ComponentModel.Composition;
+
+namespace SIUView
+{
+   [Export(typeof(IView))]
+   public class SIUView : BaseView, IView
+   {
+      /// <summary>
+      /// Constructor
+      /// </summary>
+      SIUView() : base("SIU", "SIUView") { }
+
+      /// <summary>
+      /// Creates an SIU Table instance. 
+      /// </summary>
+      /// <param name="ctx">Context for the command. </param>
+      /// <returns>new SIU table</returns>
+      protected override BaseTable CreateTableInstance(IContext ctx)
+      {
+         SIUTable SIUTable = new SIUTable(ctx, viewName);
+         SIUTable.ReadXmlFile();
+         return SIUTable;
+      }
+   }
+}
