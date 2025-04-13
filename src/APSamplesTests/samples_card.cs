81ac6bc6a870c452ecec0ee6d45c99e9c91b3ca9
davidewan <davidewan@live.com>
2025-04-13 10:11:56 -0400

    Added CardView
    Added Card View to the AP parse

---
+++ src/APSamplesTests/samples_card.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APSamples
+{
+   public class samples_card
+   {
+      public const string APLOG_CARD_READAVAILABLERAWDATA_1 =
+      @"  INFO [2025-02-03 13:40:29-113] [CardReader.ExecuteDeviceCommand] ExecuteDeviceCommand:ReadAvailableRawData, Arg:CHIP
+";
+      public const string APLOG_CARD_READAVAILABLERAWDATA_2 =
+      @"  INFO[2025-02-03 11:04:20-965] [CardReader.ExecuteDeviceCommand] m_NxCard.ReadAvailableRawData result is ReadComplete
+";
+      public const string APLOG_CARD_READAVAILABLERAWDATA_3 =
+      @"  INFO [2025-02-03 11:44:34-420] [CardReader.ExecuteDeviceCommand] m_NxCard.ReadAvailableRawData result is InvalidMedia
+";
+   }
+}
