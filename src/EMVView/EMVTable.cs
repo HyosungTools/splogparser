c6125d4db4b730979e511404eb4975cd6b0ad8bb
davidewan <davidewan@live.com>
2025-04-13 10:06:22 -0400

    Added EMVView
    Added EMVView to the AP parse

---
+++ src/EMVView/EMVTable.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Data;
+using Contract;
+using Impl;
+using LogLineHandler;
+
+namespace EMVView
+{
+   class EMVTable : BaseTable
+   {
+      /// <summary>
+      /// constructor
+      /// </summary>
+      /// <param name="ctx">Context for the command.</param>
+      /// <param name="viewName">The (unique) name of the view being created.</param>
+      public EMVTable(IContext ctx, string viewName) : base(ctx, viewName)
+      {
+      }
+
+      /// <summary>
+      /// Process one line from the merged log file. 
+      /// </summary>
+      /// <param name="logLine">logline from the file</param>
+      public override void ProcessRow(ILogLine logLine)
+      {
+         try
+         {
+            if (logLine is APLine apLogLine)
+            {
+               switch (apLogLine.apType)
+               {
+               }
+            }
+         }
+         catch (Exception e)
+         {
+            ctx.LogWriteLine("EMVTable.ProcessRow EXCEPTION:" + e.Message);
+         }
+      }
+   }
+}
