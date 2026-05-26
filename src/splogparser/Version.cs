b03fb914a755b53238448d4dbc2c483be5d15c44
davidewan <430991+DavidEwan@users.noreply.github.com>
2026-05-26 15:55:28 -0400

    Update version information to 8.2.8

--- src/splogparser/Version.cs
+++ src/splogparser/Version.cs
@@ -1,10 +1,8 @@
 ﻿// Auto-generated version file
 namespace SplogParser
 {
-   public static class VersionInfo
-   {
-      public static string Current =>
-          System.Reflection.Assembly.GetExecutingAssembly()
-              .GetName().Version.ToString();
-   }
+    public static class VersionInfo
+    {
+        public const string Current = "8.2.8";
+    }
 }
